07e450d485706ca58e2e8c056cc260fb58fa224d
Bekir Demir <bekirdemir001@gmail.com>
2023-10-03 16:05:17 +0300

    Version2

--- src/main/java/miniProjects/login/UserService.java
+++ src/main/java/miniProjects/login/UserService.java
@@ -1,6 +1,13 @@
 package miniProjects.login;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class UserService {
+    List<String> usernameList = new ArrayList<>();
+    List<String> emailList = new ArrayList<>();
+    List<String> passwordList = new ArrayList<>();
 
     public void showMenu(){
 
@@ -11,6 +18,73 @@ public class UserService {
         System.out.println("Your selection: ");
     }
 
+    public void register(){
+        //Get name
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Name and Surname");
+        String name = scanner.nextLine();
+
+        //Get username
+        String username;
+        boolean existUsername;
+        do {
+            System.out.println("Enter Your Username");
+            username = scanner.nextLine();
+            existUsername = usernameList.contains(username);
+            if (existUsername){
+                System.out.println("This username has been used before. Please try a new username");
+            }
+        }while (existUsername);
+
+        //Get email
+        String email;
+        boolean isValid;
+        boolean existEmail;
+        do {
+            System.out.println("Enter Your Email");
+            email = scanner.nextLine().trim();
+            isValid = validateEmail(email);
+
+            existEmail = emailList.contains(email);
+            if (existEmail){
+                isValid = false;
+                System.out.println("This email has been used before. Please try a new email");
+            }
+
+        }while (!isValid);
+
+    }
+
+    public static boolean validateEmail(String email){
+        boolean isValid;
+
+        boolean space = email.contains(" ");
+        boolean isContainAt = email.contains("@");
+        if (space){
+            System.out.println("Email contains no space");
+            isValid = false;
+        } else if (!isContainAt) {
+            System.out.println("Email contains '@'");
+            isValid = false;
+        }else {
+            String firstPart = email.split("@")[0];
+            String secondPart = email.split("@")[1];
+
+            boolean checkFirstPart = firstPart.replaceAll("[a-zA-Z0-9_.-]", "").length() ==0;
+            boolean checkSecondPart = secondPart.equals("gmail.com") ||
+                                        secondPart.equals("hotmail.com") ||
+                                        secondPart.equals("yahoo.com");
+
+            if (!checkFirstPart){
+                System.out.println("Email has no characters except lowercase, uppercase, numbers or symbols such as _.-");
+            } else if (!checkSecondPart) {
+                System.out.println("Email must end with gmail.com, hotmail.com or yahoo.com");
+            }
+
+            isValid = checkFirstPart && checkSecondPart;
+        }
+        return isValid;
+    }
 
 
 }
